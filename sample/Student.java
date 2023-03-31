be8c85e0a1981ce5cef7940b8626aaee4e925d28
Abhijith Alias <69385258+tron01@users.noreply.github.com>
2023-03-31 16:29:38 +0530

    Create Student.java

---
+++ sample/Student.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Student {
+    public static void main (String args[]){
+        
+        int n;
+        String temp;
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter the limit of names:");
+        n =s.nextInt();
+        String names[] = new String[n];
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter all the names: ");
+        for(int i=0; i<n;i++){
+          names[i] =s1.next();
+          // names[i] =s1.nextLine();
+
+        }
+
+        for(int i =0;i<n;i++){
+            for(int j=i+1; j<n;j++){
+                if(names[i].compareTo(names[j])>0){
+                    temp =names[i];
+                    names[i]=names[j];
+                    names[j]=temp;
+
+                }
+
+            }
+        }
+         
+         System.out.println("Names in sorted Order :");
+         for (int i =0; i<n;i++){
+                System.out.println(names[i]);
+         }
+        
+    }
+}
