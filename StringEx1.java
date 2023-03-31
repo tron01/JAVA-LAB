a83fad416ecd70fccf957699d33e62777f403414
Abhijith Alias <69385258+tron01@users.noreply.github.com>
2023-03-31 15:57:18 +0530

    Create StringEx1.java

---
+++ StringEx1.java
@@ -0,0 +1,18 @@
+class StringEx1{
+    public static void main (String args[]){
+        String s1 ="hello ";
+        String s2 ="world";
+        String s3 =new String("SNGCE");
+        
+        System.out.println(s1+" "+s2); 
+        System.out.println("Length of s1 :"+s1.length());
+        System.out.println(s1.replace('l','e')); 
+        System.out.println(s1.concat(s2)); 
+        System.out.println(s1.equals(s2)); 
+        System.out.println(s1.toUpperCase()); 
+        System.out.println(s1.substring(0,1)); 
+
+
+
+    }
+}
