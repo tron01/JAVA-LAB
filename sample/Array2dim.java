b6d2c6da8275306b3397ac975e7f0a48ad65b21b
Abhijith Alias <69385258+tron01@users.noreply.github.com>
2023-04-04 12:05:01 +0530

    Update Array2dim.java

--- sample/Array2dim.java
+++ sample/Array2dim.java
@@ -18,9 +18,12 @@ class Array2dim{
   void PrintArray(){
       System.out.println(" Elements are:");
       for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-               
-  }
+        for(int j=0;j<3;j++){           
+        System.out.print(a[i][j] +" ");
+      }
+       System.out.println();
+    }
+    }
   void FindArr(){
     int n;
     int c=0;
