d180c91983a4439dd6ac5873df499b7d0219624f
Abhijith Alias <69385258+tron01@users.noreply.github.com>
2023-04-18 09:49:45 +0530

    Constructors

---
+++ sample/Bike.java
@@ -0,0 +1,24 @@
+class Bike{
+    String name;
+    int year;
+
+    Bike(String n,int y){
+    name =n;
+    year= y; 
+    }
+    void show(){
+        System.out.println("Name :"+name);
+        System.out.println("Year:"+year);
+    }
+public static void main(String args[]){
+
+
+Bike b1 =new Bike("TVS",1990);
+b1.name ="AAA";
+b1.year =2022;
+
+b1.show();
+Bike b2=new Bike("Bmw",2001);
+b2.show();
+}
+}
