fc59bbcf7449c1d7f06d32d3fd41681463c8a890
Abhijith Alias <69385258+tron01@users.noreply.github.com>
2023-04-18 09:49:52 +0530

    Update SMatrix.java

---
+++ sample/SMatrix.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class SMatrix{
+    int a[][];
+    int n; 
+    void ReadArray(){
+     
+    System.out.println("Enter the size of the matrix:");
+    Scanner s =new Scanner(System.in);
+    n=s.nextInt();
+    a=new int[n][n];
+    System.out.println("Enter the elements  matrix:");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        a[i][j]=s.nextInt();
+
+    }
+    }
+  }
+
+    void PrintArray(){
+      
+      System.out.println("  Elements of A:");
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){           
+        System.out.print(a[i][j] +" ");
+      }
+       System.out.println();
+    }
+    }
+    public static void main(String args[]){
+SMatrix obj =new SMatrix();
+obj.ReadArray();
+obj.PrintArray();
+    }
+}
