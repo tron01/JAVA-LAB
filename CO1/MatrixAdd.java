44c3d7df0b3aebaad5c3ee69006feb587c0813ff
tron01 <69385258+tron01@users.noreply.github.com>
2023-06-12 23:41:44 +0530

    Update MatrixAdd.java

--- CO1/MatrixAdd.java
+++ CO1/MatrixAdd.java
@@ -5,32 +5,35 @@ class MatrixAdd{
     int b[][];
     int c[][];
     
-
+int size;
 void ReadM(){
-    a=new int[3][3];
-    b=new int[3][3];
+    
 
    Scanner s =new Scanner(System.in);
+    System.out.println("Enter the size of array:");
+    size=s.nextInt();
+    a=new int[size][size];
+    b=new int[size][size];
     System.out.println("Enter the elements 1st matrix:");
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
         a[i][j]=s.nextInt();
 
     }
     }
 
     System.out.println("Enter the elements 2nd matrix:");
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
         b[i][j]=s.nextInt();
 
     }
     }
 }
 void AddM(){
-    c=new int[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
+    c=new int[size][size];
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
         c[i][j]=a[i][j]+b[i][j];
 
     }
@@ -39,22 +42,22 @@ void AddM(){
 }
 void PrintM(){
  System.out.println("  Elements of A:");
-      for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){           
+      for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){           
         System.out.print(a[i][j] +" ");
       }
        System.out.println();
     }
 System.out.println("  Elements of B:");
-      for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){           
+      for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){           
         System.out.print(b[i][j] +" ");
       }
        System.out.println();
     }
 System.out.println("  Elements of C:");
-      for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){           
+      for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){           
         System.out.print(c[i][j] +" ");
       }
        System.out.println();
