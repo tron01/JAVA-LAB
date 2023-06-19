c5c5aca33187d005f73c2e145487a0a195827ad6
tron01 <69385258+tron01@users.noreply.github.com>
2023-06-19 21:51:14 +0530

    Update ComplexNo.java

--- CO1/ComplexNo.java
+++ CO1/ComplexNo.java
@@ -7,9 +7,11 @@ class Complex{
         img=i;
     }
     void display(){
-        System.out.println();
+         System.out.println("---------------------");
+        System.out.println(real+"+"+img+"i");
+        System.out.println("---------------------");
     }
-  public static Complex add(Complex n, Complex n2){
+  public static Complex add(Complex n1, Complex n2){
     Complex result =new Complex(0,0);
     result.real=n1.real+n2.real;
     result.img=n1.img +n2.img;
@@ -19,12 +21,12 @@ class Complex{
 public static void main(String[] args){
     Complex c1 =new Complex(4,6);
     Complex c2 =new Complex(4,4);
-    System.out.println("First complex number.");
+    System.out.println("First complex number");
     c1.display();
-    System.out.println("Second Complex number.");
+    System.out.println("Second Complex number");
     c2.display();
     Complex result=add(c1,c2);
-    System.out.print("Addition of two complex number")
+    System.out.println("Addition of two complex number");
     result.display();
 
 }
