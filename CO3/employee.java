666561f6c4e86befc3712441512fb315c87423bf
tron01 <69385258+tron01@users.noreply.github.com>
2023-06-26 23:03:50 +0530

    CO3 EMP update input

--- CO3/employee.java
+++ CO3/employee.java
@@ -1,21 +1,48 @@
+import java.util.Scanner;
 class employee{
 
+
     public static void main(String[] args) {
-       Teacher teacObj[] = new Teacher[2];
-       teacObj[0] = new Teacher("1","A","AA",11,"AAA","AAAA");
-       teacObj[1] = new Teacher("2","B","BB",11,"BBB","BBBB");
-       teacObj[0].display();
-       teacObj[1].display();
+    String Name,Address,Dep,sub;
+    int Empid,sal;
+ 
+    System.out.println("Enter the no. of employees");
+   
+    Scanner s = new Scanner(System.in);   
+    int n=s.nextInt();
+        Teacher e[] = new Teacher[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the Employee Id : ");
+            Empid= s.nextInt();
+            System.out.print("Enter the Employee name : ");
+            Name = s.next();
+            System.out.print("Enter the Address : ");
+            Address =s.next();
+            System.out.print("Enter the salary : ");
+            sal =s.nextInt();
+            System.out.print("Enter the Department : ");
+            Dep =s.next();
+            System.out.print("Enter the subject : ");
+            sub=s.next();
+            e[i] = new Teacher(Empid,Name,Address,sal,Dep,sub);
+            System.out.println("..........................................");
+         }
+  
+  
+    for(int i=0; i<n; i++) {
+   	e[i].display();
+     System.out.println("..........................................");
+    }
     }
 }
 
 class Employees {
-    String Empid;
+    int Empid;
     String Name;
     String Address;
     int Salary;
 
-    Employees(String id,String name,String addr,int salary){
+    Employees(int id,String name,String addr,int salary){
         this.Empid   = id;
         this.Name    = name;
         this.Address = addr;
@@ -27,6 +54,7 @@ class Employees {
         System.out.println("Address : " + this.Address);
         System.out.println("Salary  : " + this.Salary);
     }
+
 }
 
 
@@ -34,16 +62,15 @@ class Employees {
 class Teacher extends Employees{  
     String Department;
     String Subject;
-    Teacher(String id,String name,String addr,int salary,String dept,String subj){
+    Teacher(int id,String name,String addr,int salary,String dept,String subj){
         super(id,name,addr,salary);
         this.Department=dept;
         this.Subject=subj;
     }
-    void display(){
-        System.out.println("..........................................");
+    void display(){   
         super.display();
         System.out.println("Dept Name : " + this.Department);
         System.out.println("Subject Name : " + this.Subject);
-        System.out.println("..........................................");
+        
     }
 }
