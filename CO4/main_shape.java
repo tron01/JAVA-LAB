6387cf03bd67e0b5f1e6089d3030fad9dd9cc30d
tron01 <69385258+tron01@users.noreply.github.com>
2023-07-03 22:30:26 +0530

    c04 p1,p2

---
+++ CO4/main_shape.java
@@ -0,0 +1,12 @@
+import package_graphics.shape;
+
+
+public class main_shape {
+    public static void main(String []args){
+        shape Obj = new shape();
+        System.out.println(Obj.recArea(1,2));
+        System.out.println(Obj.cirArea(10));
+        System.out.println(Obj.squArea(10));
+        System.out.println(Obj.triArea(10,2));
+    }
+}
