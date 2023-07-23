85b5bc35a6c862d8c02ccb5f31014159d6d0ce06
tron01 <69385258+tron01@users.noreply.github.com>
2023-07-23 19:34:34 +0530

    Create Exception2.java

---
+++ CO4/Exception2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class MyException extends Exception {
+    public MyException(String str) {
+        System.out.println(str);
+    }
+}
+
+class Exception2 {
+    public static void main(String args[]) {
+        int no;
+        int i;
+        int temp, count = 0;
+        int sum = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Total numbers");
+        no = sc.nextInt();
+        for (i = 0; i < no; i++) {
+            try {
+                temp = sc.nextInt();
+                if (temp > 0) {
+                    sum += temp;
+                    count += 1;
+                } else {
+                    throw new MyException("Number is Negative");
+                }
+            } catch (MyException ex) {
+                System.out.println("EXCEPTION OCCURED -ve number entered");
+            }
+        }
+        System.out.print("Average : ");
+        System.out.println(sum / count);
+
+    }
+}
