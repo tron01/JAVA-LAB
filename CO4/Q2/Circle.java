ef1fba945361c38946cb7f8dedfc8a94eb264c48
tron01 <69385258+tron01@users.noreply.github.com>
2023-09-07 22:17:27 +0530

    shape

--- CO4/Q2/Circle.java
+++ CO4/Q2/Circle.java
@@ -1,10 +1,7 @@
 //c04 - prgm 1 part -1
 package graphics;
 import java.util.*;
-interface Shape {
-    public void area();
-    
-}
+
 
 public class Circle implements Shape {
     Scanner sc = new Scanner(System.in);
---
+++ CO4/Q2/Shape.java
@@ -0,0 +1,7 @@
+//c04 - prgm 1 part -1
+package graphics;
+import java.util.*;
+interface Shape {
+    public void area();
+    
+}
